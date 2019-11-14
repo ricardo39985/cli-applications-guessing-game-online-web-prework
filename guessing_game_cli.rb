079245fc6ece686870e067bc6a919158dbe945c1
Ricardo Persaud <ricardo39985@gmail.com>
2019-11-14 22:48:58 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,7 +1,7 @@
 # Code your solution here!
 puts 'Please enter your guess'
 def run_guessing_game
-  computers_guess = rand(1..5)
+  computers_guess = rand(2..5)
   user_input = gets.chomp
   if user_input == 'exit'
     puts 'Goodbye!'
